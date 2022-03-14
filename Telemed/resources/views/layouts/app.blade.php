ec8b7cb0593e9406ae0a887ba7041da6d23958c0
ayoubms8 <ayoub.lemsafi10@gmail.com>
2022-03-14 19:50:53 +0100

    bruh

--- Telemed/resources/views/layouts/app.blade.php
+++ Telemed/resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <div><img src ="/svg/1logo.png" alt =""></div>
+                    <div>Telemedecine</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
